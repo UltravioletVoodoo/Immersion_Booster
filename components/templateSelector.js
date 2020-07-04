278f3945710d645681aec72a374abe12066c82fa
Jonathan <jonbezeau@gmail.com>
2020-07-04 16:56:43 -0700

    Implemented MVP campaign import/export

--- components/templateSelector.js
+++ components/templateSelector.js
@@ -1,16 +1,35 @@
 import { useState, useEffect } from "react"
 import Link from "next/link"
 import PreviewSet from "./previewSet"
+import downloadTxt from "../util/downloadTxt"
 
 
 export default function TemplateSelector(props) {
     const { state, setState } = props
     const [campaign, setCampaign] = useState(null)
+    let fileReader
     
     
     function loadCampaign() {
         if (typeof window === 'undefined') return null
         setCampaign(JSON.parse(localStorage.getItem('campaign')))
+    }
+
+    function handleFileRead() {
+        const content = fileReader.result
+        console.log(content)
+        localStorage.setItem('campaign', content)
+    }
+
+    function onFileChange(file) {
+        fileReader = new FileReader()
+        fileReader.onloadend = handleFileRead
+        fileReader.readAsText(file)
+    }
+
+    function exportCampaign() {
+        const fileName = campaign.name === '' ? 'campaign' : campaign.name 
+        downloadTxt(fileName, JSON.stringify(campaign))
     }
 
     useEffect(() => {
@@ -30,8 +49,10 @@ export default function TemplateSelector(props) {
             </div>
             <div className='templateSelectorBase campaignControls'>
                 <Link href='/admin/campaignEditor'>
-                    <button>Edit/Upload Campaigns</button>
+                    <button>Edit Campaign</button>
                 </Link>
+                <input type='file' onChange={ e => onFileChange(e.target.files[0])}></input>
+                <button onClick={exportCampaign}>Export Campaign</button>
             </div>
             <style jsx>{`
                 .templateSelectorBase {
