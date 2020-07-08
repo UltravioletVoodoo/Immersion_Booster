114e560cccc86f79bd0576796314cab8b67a309e
Jonathan <jonbezeau@gmail.com>
2020-07-08 11:21:36 -0700

    Added exit button to modal

--- components/modal.js
+++ components/modal.js
@@ -19,7 +19,10 @@ export default function Modal() {
         <>
             {content && (
                 <div className='modalContainer'>
-                    {content}
+                    <img className='exit' src='/exit-door.svg' onClick={clearModal}></img>
+                    <div className='contentContainer'>
+                        {content}
+                    </div>
                 </div>
             )}
             <style jsx>{`
@@ -30,9 +33,24 @@ export default function Modal() {
                     top: 20vh;
                     height: 60vh;
                     background-color: white;
-                    border: 10px solid black;
+                    box-shadow: 0 0 20px #000;
                     border-radius: 50px;
                     z-index: 1000;
+                }
+                .contentContainer {
+                    width: calc(100% - 50px);
+                    height: calc(100% - 100px);
+                    top: 50px;
+                    margin: 25px;
+                    position: relative;
+                }
+                .exit {
+                    position: absolute;
+                    width: 50px;
+                    height: 50px;
+                    top: 0;
+                    right: 0;
+                    cursor: pointer;
                 }
             `}</style>
         </>
