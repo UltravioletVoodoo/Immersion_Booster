6a7ba1e16f11c37184c8cc03fef09cb00943638d
Jonathan <jonbezeau@gmail.com>
2020-07-16 20:38:20 -0700

    Implemented MVP for index page

--- components/chooseSiteBtn.js
+++ components/chooseSiteBtn.js
@@ -1,27 +1,38 @@
-import css from "styled-jsx/css"
 import Link from 'next/link'
-
-const buttonStyle = css`
-button {
-    width: 40vw;
-    height: 20vh;
-    font-size: 30px;
-}
-
-button:hover {
-    color: green;
-    border: 1px solid green;
-}
-`
 
 export default function ChooseSiteBtn(props) {
     const { text, href } = props
     return (
         <>
             <Link href={href}>
-                <button>{text}</button>
+                <span className='ChooseSiteBtn'>
+                    <div className='ChooseSiteBtn_Text'>
+                        {text}
+                    </div>
+                </span>
             </Link>
-            <style jsx>{buttonStyle}</style>
+            <style jsx>{`
+                .ChooseSiteBtn {
+                    position: absolute;
+                    width: 100px;
+                    height: 50px;
+                    background-color: cyan;
+                    text-align: center;
+                    border-radius: 30px;
+                    border: 2px solid brown;
+                    opacity: 0.1;
+                    transition: 0.5s;
+                }
+                .ChooseSiteBtn:hover {
+                    opacity: 1;
+                }
+                .ChooseSiteBtn_Text {
+                    position: absolute;
+                    top: 50%;
+                    transform: translateY(-50%);
+                    width: 100%;
+                }
+            `}</style>
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -12,14 +12,41 @@ const indexStyle = css`
 
 export default function Index() {
     return (
-        <div className="centered">
+        <div className="centered pageContainer">
             <Base />
             <Head><title>Immersion Booster</title></Head>
 
             <h1>Hello World</h1>
-            <ChooseSiteBtn text="Admin Site" href="/admin" />
-            <ChooseSiteBtn text="Viewer Site" href="/viewer" />
-            <style jsx>{indexStyle}</style>
+            <div className='btn adminBtn'>
+                <ChooseSiteBtn text="Admin Site" href="/admin" />
+            </div>
+            <div className='btn viewerBtn'>
+                <ChooseSiteBtn text="Viewer Site" href="/viewer" />
+            </div>
+            <style jsx>{`
+                .centered {
+                    text-align: center;
+                }
+                .pageContainer {
+                    position: absolute;
+                    left: 0;
+                    top: 0;
+                    width: 100vw;
+                    height: 100vh;
+                    background-image: url("/ImmersionBoosterArt.png");
+                    background-size: 100vw 100vh;
+                }
+                .btn {
+                    position: absolute;
+                    top: 60vh;
+                }
+                .adminBtn {
+                    left: 33vw;
+                }
+                .viewerBtn {
+                    left: 61vw;
+                }
+            `}</style>
         </div>
     )
 }
