5e4d28a18d2b1c1e252b56304cef8eaca29ff981
Jonathan <jonbezeau@gmail.com>
2020-07-20 15:23:08 -0700

    Added css scaling to index image

--- components/chooseSiteBtn.js
+++ components/chooseSiteBtn.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 
 export default function ChooseSiteBtn(props) {
     const { text, href } = props
+
     return (
         <>
             <Link href={href}>
@@ -14,14 +15,15 @@ export default function ChooseSiteBtn(props) {
             <style jsx>{`
                 .ChooseSiteBtn {
                     position: absolute;
-                    width: 100px;
-                    height: 50px;
+                    width: 100%;
+                    height: 100%;
                     background-color: cyan;
                     text-align: center;
                     border-radius: 30px;
                     border: 2px solid brown;
                     opacity: 0.1;
                     transition: 0.5s;
+                    cursor: pointer;
                 }
                 .ChooseSiteBtn:hover {
                     opacity: 1;
@@ -31,6 +33,17 @@ export default function ChooseSiteBtn(props) {
                     top: 50%;
                     transform: translateY(-50%);
                     width: 100%;
+                    font-size: 16px;
+                }
+                @media screen and (max-width: 565px) {
+                    .ChooseSiteBtn_Text {
+                        font-size: 12px;
+                    }
+                }
+                @media screen and (max-width: 400px) {
+                    .ChooseSiteBtn_Text {
+                        font-size: 10px;
+                    }
                 }
             `}</style>
         </>
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import ChooseSiteBtn from '../components/chooseSiteBtn'
 import Head from 'next/head'
 import Base from '../components/base'
 import css from "styled-jsx/css"
+import { useState, useEffect } from 'react'
 
 
 const indexStyle = css`
@@ -11,42 +12,74 @@ const indexStyle = css`
 `
 
 export default function Index() {
-    return (
-        <div className="centered pageContainer">
-            <Base />
-            <Head><title>Immersion Booster</title></Head>
+    const [dimensions, setDimensions] = useState({x: 1, y: 1})
 
-            <h1>Hello World</h1>
-            <div className='btn adminBtn'>
-                <ChooseSiteBtn text="Admin Site" href="/admin" />
-            </div>
-            <div className='btn viewerBtn'>
-                <ChooseSiteBtn text="Viewer Site" href="/viewer" />
+    useEffect(() => {
+        function handleResize() {
+            if (typeof window === 'undefined') return
+            setDimensions({x: window.innerWidth, y: window.innerHeight})
+        }
+        handleResize()
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
+    const scaleFactor = Math.min(dimensions.x / 1920, dimensions.y / 1080)
+    const width = 1920 * scaleFactor
+    const height = 1080 * scaleFactor
+
+    return (
+        <>
+            <div className='background'></div>
+            <div className="centered pageContainer">
+                <Base />
+                <Head><title>Immersion Booster</title></Head>
+                <div className='btn adminBtn'>
+                    <ChooseSiteBtn text="Admin" href="/admin" />
+                </div>
+                <div className='btn viewerBtn'>
+                    <ChooseSiteBtn text="Viewer" href="/viewer" />
+                </div>
             </div>
             <style jsx>{`
-                .centered {
-                    text-align: center;
-                }
-                .pageContainer {
+                .background {
                     position: absolute;
                     left: 0;
                     top: 0;
                     width: 100vw;
                     height: 100vh;
+                    background-color: darkcyan;
+                }
+                .centered {
+                    text-align: center;
+                }
+                .pageContainer {
+                    position: absolute;
+                    left: 50%;
+                    top: 50%;
+                    transform: translate(-50%,-50%);
+                    width: ${width}px;
+                    height: ${height}px;
                     background-image: url("/ImmersionBoosterArt.png");
-                    background-size: 100vw 100vh;
+                    background-size: ${width}px ${height}px;
+                    background-color: darkslategray;
                 }
                 .btn {
                     position: absolute;
-                    top: 60vh;
+                    top: 60%;
+                    width: 10%;
+                    height: 10%;
                 }
                 .adminBtn {
-                    left: 33vw;
+                    left: 27%;
                 }
                 .viewerBtn {
-                    left: 61vw;
+                    left: 53%;
                 }
             `}</style>
-        </div>
+        </>
     )
 }
